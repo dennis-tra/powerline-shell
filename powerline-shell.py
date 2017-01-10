4877b0cb413c194080edb9d050fbc019047ecbac
Dennis Trautwein <dennis.trautwein@posteo.de>
2017-01-10 11:05:32 +0100

    Remove exit code from powerline

--- powerline-shell.py
+++ powerline-shell.py
@@ -649,15 +649,6 @@ def add_jobs_segment(powerline):
 
 
 add_jobs_segment(powerline)
-def add_exit_code_segment(powerline):
-    if powerline.args.prev_error == 0:
-        return
-    fg = Color.CMD_FAILED_FG
-    bg = Color.CMD_FAILED_BG
-    powerline.append(' %s ' % str(powerline.args.prev_error), fg, bg)
-
-
-add_exit_code_segment(powerline)
 def add_root_segment(powerline):
     root_indicators = {
         'bash': ' \\$ ',
